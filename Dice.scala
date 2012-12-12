01a55d58833684a2dabfed225cceda742fa3d17c
Avi Knoll <ajk@escher.l4verified.nicta.com.au>
2012-12-12 16:24:20 +1100

    worked out multiline expr

--- Dice.scala
+++ Dice.scala
@@ -4,5 +4,8 @@ import scala.util.parsing.combinator._
 class Dice extends RegexParsers {
   val natural : Parser[Int] = regex("""\d+""".r) ^^ {s => s.toInt}
 
-  def roll : Parser[Any] = opt(natural) ~ ("d" | "D") ~ natural
+  def roll : Parser[Int, Int] = ( opt(natural) ^^ {o => getOrElse(1)} 
+                                ~ ("d" | "D") 
+                                ~ natural
+                                )
 }
