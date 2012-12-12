eaafda002de97e33d84072a511ee7b52c7c2f71f
Avi Knoll <ajk@escher.l4verified.nicta.com.au>
2012-12-12 16:16:41 +1100

    type checks out

--- Dice.scala
+++ Dice.scala
@@ -2,7 +2,7 @@ import scala.util.parsing.combinator._
 
 
 class Dice extends RegexParsers {
-  val natural : Parser[Int] = regex("""\d+""".r)
+  val natural : Parser[Int] = regex("""\d+""".r) ^^ {s => s.toInt}
 
   def roll : Parser[Any] = opt(natural) ~ ("d" | "D") ~ natural
 }
