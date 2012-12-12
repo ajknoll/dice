87539fe532994f49ff347a7b4e99e9844a03d225
Avi Knoll <Avi.Knoll@nicta.com.au>
2012-12-12 17:08:50 +1100

    type issues: don't understand type sig from parser lib

--- Dice.scala
+++ Dice.scala
@@ -6,8 +6,8 @@ class Dice extends RegexParsers {
   val natural : Parser[Int] = regex("""\d+""".r) ^^ {s => s.toInt}
 
   def roll : Parser[(Int, Int)] =
-    ( opt(natural) ^^ {o : Option[Int] => o.getOrElse {_ : Int => 1}} 
+    ( (opt(natural) ^^ {o : Option[Int] => o.getOrElse {_ : Int => 1}})
     ~ ("d" | "D") 
-    ~> natural
-    )
+    ~ natural
+    ) ^^ {(count : Int, _ : Any, sides : Int) => (count, sides)}
 }
