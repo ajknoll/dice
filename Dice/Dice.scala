026b28c83bb5c129d147bec089e5311dac528fa8
Avi Knoll <Avi.Knoll@nicta.com.au>
2012-12-13 12:52:23 +1100

    Figured out use of case to match result of parse

--- Dice/Dice.scala
+++ Dice/Dice.scala
@@ -1,13 +1,21 @@
 import scala.util.parsing.combinator._
 
 class Dice extends RegexParsers {
-  val natural : Parser[Int] = regex("""\d+""".r) ^^ {s => s.toInt}
+  sealed val natural : Parser[Int] = regex("""\d+""".r) ^^ {s => s.toInt}
+  sealed def getOr1 (o : Option[Int]) : Int = {
+    case i : Some[Int] => i.get
+    case _ : None => 1
+  }
 
   def roll : Parser[(Int, Int)] =
-    ( (opt(natural) ^^ {o : Option[Int] => o.getOrElse {_ : Int => 1}})
-    ~ ("d" | "D") 
+    ( (opt(natural) ^^ {o : Option[Int] => getOr1(o)})
+    ~ ("d" | "D")
     ~ natural
-    ) ^^ {(count : Int, _ : String, sides : Int) => (count, sides)}
+    //) ^^ {(count : Int, _ : String, sides : Int) => (count, sides)}
+    ) ^^ {case (count: Int) ~ _ ~ (sides : Int) => (count, sides)}
+
+  def repeats : Parser[Int] =
+    opt(natural) ^^ {o : Option[Int] => getOr1(o)}
 
   def modifier : Parser[(Int => Int)] =
     (
