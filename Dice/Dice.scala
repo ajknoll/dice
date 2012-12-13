83fdcd2fbe474f2ac6fc3df794c95e13de2b309e
Avi Knoll <Avi.Knoll@nicta.com.au>
2012-12-13 13:51:58 +1100

    scalac is good, ideac is useless

--- Dice/Dice.scala
+++ Dice/Dice.scala
@@ -1,10 +1,10 @@
 import scala.util.parsing.combinator._
 
 class Dice extends RegexParsers {
-  sealed val natural : Parser[Int] = regex("""\d+""".r) ^^ {s => s.toInt}
-  sealed def getOr1 (o : Option[Int]) : Int = {
-    case i : Some[Int] => i.get
-    case _ : None => 1
+  val natural : Parser[Int] = regex("""\d+""".r) ^^ {s => s.toInt}
+  def getOr1 (o : Option[Int]) : Int = o match {
+    case Some(i) => i
+    case None    => 1
   }
 
   def roll : Parser[(Int, Int)] =
@@ -20,10 +20,18 @@ class Dice extends RegexParsers {
   def modifier : Parser[(Int => Int)] =
     (
       (("+" | "-" | "*" | "/") ~ natural)
-      ^^ { case ("+", y) => {x : Int => x + y}
-           case ("-", y) => {x : Int => x - y}
-           case ("*", y) => {x : Int => x * y}
-           case ("/", y) => {x : Int => x / y}
+      ^^ { case ("+" ~ y) => {x : Int => x + y}
+           case ("-" ~ y) => {x : Int => x - y}
+           case ("*" ~ y) => {x : Int => x * y}
+           case ("/" ~ y) => {x : Int => x / y}
          }
     )
+}
+
+object ParseDice extends Dice {
+  def main (args : Array[String]) {
+    for (dice <- args) {
+      val parsed = parseAll(roll, dice)
+    }
+  }
 }
