b133fa07da60ee4fcf34d714aa388bb4e5f6eb73
Avi Knoll <Avi.Knoll@nicta.com.au>
2012-12-14 11:16:34 +1100

    working neater version

--- Dice/Dice.scala
+++ Dice/Dice.scala
@@ -32,7 +32,7 @@ class Dice extends RegexParsers {
          }
     )
 
-  def evaluate (count : Int, sides : Int, mods : List[Int => Int]) : (Int, List[Int]) =
+  def evaluate (count : Int) (sides : Int) (mods : List[Int => Int]) : (Int, List[Int]) =
     { val rand = Random
       val results = for (_ <- 1 to count) yield (rand.nextInt(sides) + 1)
       ((results.sum /: mods) ((acc, m) => m(acc)), results.toList)
@@ -45,7 +45,7 @@ object ParseDice extends Dice {
       parseAll(diceRoll, a) match {
         case Success(r, _) => {
           val repeats = r._1._1
-          val (outcome, rolls) = evaluate(r._1._2, r._2)
+          val (outcome, rolls) = evaluate (r._1._2._1) (r._1._2._2) (r._2)
           println(i.toString ++ ": "
                ++ outcome.toString
                ++ " [" ++ rolls.toString 
